51018b78baaa58fe18eb77788b54fcd1aa688071
shldecke <shldecke@ucsc.edu>
2023-04-26 13:00:51 -0700

    lab5 done :-):

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,9 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5: Data Types and Variables
+// Author: Shelby Decker
+// Date: 4/26/2023
 
-// Constants
+// Variables
+var make = "Fiat";
+var model = "500 Sport";
+var year = "2012";
+var color = "red";
+var currentYear = 2023;
 
+// calculate
+var age = currentYear - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br><br>");
+
+
+/*
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
@@ -19,3 +36,7 @@ function main() {
 
 // let's get this party started
 main();
+*/
+
+// in order to automatically comment a line, simply navigate to that line and command + slash
+// if you want to auto comment multiple lines, simply select them and command + slash
