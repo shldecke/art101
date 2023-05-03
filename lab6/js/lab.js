81a925b0f72b237ec131e52ed1a48784a874a388
shldecke <shldecke@ucsc.edu>
2023-05-03 16:22:05 -0700

    whoops, forgot to do this sooner

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,9 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6: Arrays & Objects
+// Author: Shelby Decker
+// Due Date: 5/2/23 7:00a
 
-// Constants
+// Arrays/Objects
 
+var myTransport = ['feet', 'car', 'bus', 'skateboard'];
+var myMainRide = {
+  make: "Fiat", 
+  mode: "500 Sport",
+  year: 2012,
+  color: "red",
+  currentYear: 2023,
+  age: function() {
+    return this.currentYear - this.age;
+  } 
+};
+
+// output
+document.writeln("Getting around: " + myTransport[0], ", " + myTransport[1] + ", " + myTransport[2] + ", " + myTransport[3] + "<br>");
+// this little trick allows us to write an object to the document
+document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre>");
+/*
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
@@ -18,4 +35,4 @@ function main() {
 }
 
 // let's get this party started
-main();
+main(); */
---
+++ lab7/js/lab.js
@@ -0,0 +1,19 @@
+// index.js - Lab 7: 
+// Author: Shelby Decker
+// Due Date: 5/2/23 7:00a
+
+// Functions
+/*
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main(); */
