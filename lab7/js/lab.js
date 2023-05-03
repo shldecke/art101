df3517d6e3acf770596a7c6384b5d164742989bb
shldecke <shldecke@ucsc.edu>
2023-05-03 16:59:02 -0700

    got the javascript working with a popup box :)

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,19 +1,31 @@
-// index.js - Lab 7: 
+// index.js - Lab 7: Functions
 // Author: Shelby Decker
-// Due Date: 5/2/23 7:00a
+// Due Date: 5/4/23 7:00a
 
 // Functions
-/*
+
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
+function myFunction() {
   // some code here
-  // return results;
+  // take input from user
+  var userName = window.prompt("Hello. What is your name?", "null");
+  // split the name given so it's in array form
+  var splitUser = userName.split("");
+  // sort the split array
+  splitUser.sort();
+  // return the joined, sorted, results
+  return splitUser.join("");
 }
 
+// calling the function
+var userInput = myFunction();
+document.writeln(userInput);
+/*
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
 }
 
 // let's get this party started
-main(); */
+main();
+*/
