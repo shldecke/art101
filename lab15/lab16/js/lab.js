86bf022a980bedc40d085840212c4ba3a1520137
shldecke <shldecke@ucsc.edu>
2023-06-01 08:51:59 -0700

    redid lab16

---
+++ lab15/lab16/js/lab.js
@@ -0,0 +1,26 @@
+// index.js - Lab 15: AJAX
+// Author: Shelby Decker
+// Date: 6/1/23
+
+// today we will be trying to connect with an API
+function getStuff(){
+    $.ajax({
+        // URL
+        url: "https://yesno.wtf/api",
+        // data to be sent
+        data: {},
+        type: "GET",
+        dataType: "application/json" 
+    })
+    .done(function(data){
+        //console.log(data.answer);
+        //console.log(data.image);
+        $("#output").html("<h2>${data.answer}</h2><img src='${data.image}'>"); 
+        //$("#output").html += "<img src=" + data.image + ">";
+        })
+    .fail(function(jqXHR, textStatus, errorThrown){
+        console.log("Error: ", textStatus, errorThrown);
+        })
+}
+
+$("#activate").click(getStuff());
