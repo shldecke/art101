8692d394c42f2d4c754616baa3f37991c0c529e5
shldecke <shldecke@ucsc.edu>
2023-06-04 16:19:29 -0700

    trying again

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,26 +1,28 @@
-// index.js - Lab 15: AJAX
+// index.js - Lab 16: JSON and APIs
 // Author: Shelby Decker
-// Date: 6/1/23
+// Date: 6/6/23
 
-// today we will be trying to connect with an API
-function getStuff(){
-    $.ajax({
-        // URL
-        url: "https://yesno.wtf/api",
-        // data to be sent
-        data: {},
-        type: "GET",
-        dataType: "application/json" 
-    })
-    .done(function(data){
-        //console.log(data.answer);
-        //console.log(data.image);
-        $("#output").html("<h2>${data.answer}</h2><img src='${data.image}'>"); 
+// uses .ajax to retreive data and store object in a variable comicObj
+// - give the section a title using the comicObj.title key from the object
+// - create an image tag using comicObj.img key 
+// - give the image an alt and title attributes using the comicObj.alt key
+
+$.ajax({
+    // URL
+    url: "https://xkcd.com/614/info.0.json",  // delete /614/
+    // data to be sent
+    data: {},
+    type: "GET",
+    dataType: "application/json" 
+})
+.done(function(data){
+    //console.log(data.answer);
+    //console.log(data.image);
+    var comicObj = data;
+    $("#output").html("our comicObj: ", JSON.stringify(comicObj));
+        //.html("<h2>${data.answer}</h2><img src='${data.image}'>"); 
         //$("#output").html += "<img src=" + data.image + ">";
-        })
-    .fail(function(jqXHR, textStatus, errorThrown){
-        console.log("Error: ", textStatus, errorThrown);
-        })
-}
-
-$("#activate").click(getStuff());
+    })
+.fail(function(jqXHR, textStatus, errorThrown){
+    console.log("Error: ", textStatus, errorThrown);
+    })
